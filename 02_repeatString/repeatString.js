073aaf13907714cc8435f2eee9e0d0612511fc24
Grace Zhen <grace.zhen00@gmail.com>
2022-10-01 16:29:03 -0700

    finished 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "No negative numbers";
+    }
+    let total = '';
+    for (let i = 0; i < num; i++) {
+        total += string;
+    }
+    return total;
 };
 
 // Do not edit below this line
